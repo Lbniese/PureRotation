bf467718b3b79f99305b8fdd6a895f6c9d0605e0
sr55371@gmail.com <sr55371@gmail.com@b0d4439c-6da0-d869-d809-20cad30054f5>
2013-07-01 05:22:44 +0000

    fixed a typo in druid and tweaked BM monk

--- trunk/AdvancedAI/Class/Monk/PvE/BrewmasterMonk.cs
+++ trunk/AdvancedAI/Class/Monk/PvE/BrewmasterMonk.cs
@@ -15,6 +15,9 @@ namespace AdvancedAI.Spec
         //public override WoWClass Class { get { return WoWClass.Monk; } }
         //public override WoWSpec Spec { get { return WoWSpec.MonkBrewmaster; } }
         static LocalPlayer Me { get { return StyxWoW.Me; } }
+        private static double? _time_to_max;
+        private static double? _EnergyRegen;
+        private static double? _energy;
 
         public static Composite CreateBMCombat
         {
@@ -22,10 +25,10 @@ namespace AdvancedAI.Spec
             {
                 return new PrioritySelector(
                     /*Things to fix
-                     * energy capping
+                     * energy capping - fixed (stole alex's code)
                      * need to check healing spheres 
                      * need to work on chi wave to get more dmg/healing out it
-                     * chi capping? need to do more checking
+                     * chi capping? need to do more checking - fixed as far as i know
                     */
                     Spell.Cast("Spear Hand Strike", ret => StyxWoW.Me.CurrentTarget.IsCasting && StyxWoW.Me.CurrentTarget.CanInterruptCurrentSpellCast),
 
@@ -42,8 +45,6 @@ namespace AdvancedAI.Spec
                     //// apply the Weakened Blows debuff. Keg Smash also generates allot of threat 
                     Spell.Cast("Keg Smash", ctx => Me.CurrentChi <= 2
                         && Clusters.GetCluster(Me, Unit.NearbyUnfriendlyUnits, ClusterType.Radius, 8).Any(u => !u.HasAura("Weakened Blows"))),
-
-                    //Spell.Cast("Jab", ret => Me.CurrentEnergy >= 80 && Me.HealthPercent >= 35),
 
                     //PB, EB, and Guard are off the GCD
                     //!!!!!!!Purifying Brew !!!!!!!
@@ -71,29 +72,27 @@ namespace AdvancedAI.Spec
 
                     Spell.Cast("Blackout Kick", ret => Me.CurrentChi >= 3),
 
-                    Spell.Cast("Keg Smash", ctx => Me.CurrentChi <= 2),
-
-                    Spell.Cast("Jab", ret => Me.CurrentEnergy >= 76),
+                    Spell.Cast("Keg Smash", ret => Me.CurrentChi <= 3),
 
                     //Chi Talents
                     //need to do math here and make it use 2 if im going to use it
-                    //Spell.Cast("Zen Sphere", ret => !Me.HasAura("Zen Sphere")),
                     Spell.Cast("Chi Wave", on => Me, ret => Me.HealthPercent <= 85),
                     Spell.Cast("Zen Sphere", on => _tanking),
 
                     Spell.Cast("Expel Harm", ret => Me.HealthPercent <= 90),
 
                     //Healing Spheres need to work on
-                    Spell.CastOnGround("Healing Sphere", on => Me.Location, ret => Me.HealthPercent <= 50 && Me.CurrentEnergy >= 75),
+                    Spell.CastOnGround("Healing Sphere", on => Me.Location, ret => Me.HealthPercent <= 50 && Me.CurrentEnergy >= 60),
 
                     Spell.Cast("Spinning Crane Kick", ret => Unit.NearbyUnfriendlyUnits.Count(u => u.DistanceSqr <= 8 * 8) >= 5 && SpellManager.Spells["Keg Smash"].CooldownTimeLeft.TotalSeconds > 2),
 
-                    Spell.Cast("Jab", ret => SpellManager.Spells["Keg Smash"].CooldownTimeLeft.TotalSeconds > 3),
+                    Spell.Cast("Jab", ret => time_to_max <= 1 || SpellManager.Spells["Keg Smash"].CooldownTimeLeft.TotalSeconds > 3),
 
+                    Spell.CastOnGround("Summon Black Ox Statue", on => Me.CurrentTarget.Location, ret => !Me.HasAura("Sanctuary of the Ox") && Me.CurrentTarget.IsBoss),
                     //dont like using this in auto to many probs with it
                     //Spell.Cast("Invoke Xuen, the White Tiger", ret => Me.CurrentTarget.IsBoss && IsCurrentTank()),
 
-                    Spell.Cast("Tiger Palm", ret => SpellManager.Spells["Keg Smash"].CooldownTimeLeft.TotalSeconds > 1)
+                    Spell.Cast("Tiger Palm")
                         );
             }
         }
@@ -120,6 +119,53 @@ namespace AdvancedAI.Spec
             {
                 var _tank = Group.Tanks.FirstOrDefault(u => StyxWoW.Me.CurrentTarget.ThreatInfo.TargetGuid == u.Guid && u.HealthPercent < 90 && u.Distance < 40);
                 return _tank;
+            }
+        }
+        #endregion
+
+        #region Energy Crap
+
+        protected static double EnergyRegen
+        {
+            get
+            {
+                if (!_EnergyRegen.HasValue)
+                {
+                    _EnergyRegen = Styx.WoWInternals.Lua.GetReturnVal<float>("return GetPowerRegen()", 1);
+                    return _EnergyRegen.Value;
+                }
+                return _EnergyRegen.Value;
+            }
+        }
+        protected static double energy
+        {
+            get
+            {
+                if (!_energy.HasValue)
+                {
+                    _energy = Styx.WoWInternals.Lua.GetReturnVal<int>("return UnitPower(\"player\");", 0);
+                    return _energy.Value;
+                }
+                return _energy.Value;
+            }
+        }
+        private static RunStatus resetVariables()
+        {
+            _time_to_max = null;
+            _energy = null;
+            _EnergyRegen = null;
+            return RunStatus.Failure;
+        }
+        protected static double time_to_max
+        {
+            get
+            {
+                if (!_time_to_max.HasValue)
+                {
+                    _time_to_max = (100 - energy) * (1.0 / EnergyRegen);
+                    return _time_to_max.Value;
+                }
+                return _time_to_max.Value;
             }
         }
         #endregion
