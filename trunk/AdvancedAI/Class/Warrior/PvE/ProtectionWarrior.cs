44aa392402200580732e31e1233cb815aecbfafa
sr55371@gmail.com <sr55371@gmail.com@b0d4439c-6da0-d869-d809-20cad30054f5>
2013-08-29 06:04:00 +0000

    Started working on ProtectionWarrior.cs looks good so far

--- trunk/AdvancedAI/Class/Warrior/PvE/ProtectionWarrior.cs
+++ trunk/AdvancedAI/Class/Warrior/PvE/ProtectionWarrior.cs
@@ -1,19 +1,20 @@
-﻿using CommonBehaviors.Actions;
+﻿using System.Windows.Forms;
+using AdvancedAI.Helpers;
+using AdvancedAI;
+using CommonBehaviors.Actions;
 using Styx;
 using Styx.Common;
-using Styx.CommonBot;
-using Styx.Helpers;
 using Styx.TreeSharp;
 using Styx.WoWInternals;
 using Styx.WoWInternals.WoWObjects;
-using AdvancedAI.Helpers;
-
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Action = Styx.TreeSharp.Action;
+using AdvancedAI.Managers;
+using Styx.CommonBot;
 
 namespace AdvancedAI.Spec
 {
@@ -27,15 +28,39 @@ namespace AdvancedAI.Spec
                 return new PrioritySelector(
                     new Decorator(ret => AdvancedAI.PvPRot,
                         ProtectionWarriorPvP.CreatePWPvPCombat),
-                    new Decorator(ret => Unit.UnfriendlyMeleeUnits.Count() > 2,
-                        CreateAoe()),    
-                        
+                    new Decorator(ret => Me.HasAura("Dire Fixation"),
+                        new PrioritySelector(
+                            Class.BossMechs.HorridonHeroic())),
+                    new Throttle(1,1,
+                        new PrioritySelector(
+                    Common.CreateInterruptBehavior())),
+
+                    new Decorator(ret => AdvancedAI.Burst,
+                        new PrioritySelector(
+                    Spell.Cast("Recklessness", ret => Me.CurrentTarget.IsWithinMeleeRange),
+                    Spell.Cast("Bloodbath", ret => Me.CurrentTarget.IsWithinMeleeRange),
+                    Spell.Cast("Avatar", ret => Me.HasAura("Recklessness") && Me.CurrentTarget.IsWithinMeleeRange),
+                    Spell.Cast("Skull Banner", ret => Me.HasAura("Recklessness") && Me.CurrentTarget.IsWithinMeleeRange))),
+                    new Action(ret => { Item.UseHands(); return RunStatus.Failure; }),
+
+                    Spell.Cast("Demoralizing Shout", ret => Unit.NearbyUnfriendlyUnits.Count(u => u.DistanceSqr <= 10 * 10) >= 1),
+                    Spell.Cast("Shield Block", ret => Me.CurrentRage > 60),
+
+                    new Decorator(ret => Unit.NearbyUnfriendlyUnits.Count(u => u.DistanceSqr <= 8 * 8) >= 2,
+                        CreateAoe()),
+
+                    DemoBanner(),
+                    HeroicLeap(),
+                    MockingBanner(),   
+                    
                     Spell.Cast("Shield Slam"),
-                    Spell.Cast("Revenge", ret => Me.RagePercent < 90),
-                    Spell.Cast("Devastate"),
+                    Spell.Cast("Revenge", ret => Me.CurrentRage < 90),
+                    Spell.Cast("Dragon Roar", ret =>  Me.CurrentTarget.Distance <= 8),
+                    Spell.Cast("Execute"),
                     Spell.Cast("Thunder Clap", ret => Me.CurrentTarget.HasAura("Weakened Blows")),
                     Spell.Cast("Commanding Shout"),
-                    Spell.Cast("Heroic Strike", ret => Me.RagePercent > 85));
+                    Spell.Cast("Heroic Strike", ret => Me.CurrentRage > 85 || Me.HasAura(122510) || Me.HasAura(122016)),
+                    Spell.Cast("Devastate"));
             }
         }
 
@@ -52,9 +77,51 @@ namespace AdvancedAI.Spec
         private static Composite CreateAoe()
         {
             return new PrioritySelector(
+                Spell.Cast("Thunder Clap"),
+                Spell.Cast("Cleave", ret => Me.CurrentRage > 85 || Me.HasAura(122510) || Me.HasAura(122016)),
                 Spell.Cast("Thunder Clap", ret => Me.CurrentTarget.HasAura("Weakened Blows"))
                 );
         }
+
+        private static Composite HeroicLeap()
+        {
+            return
+                new Decorator(ret => SpellManager.CanCast("Heroic Leap") &&
+                    Lua.GetReturnVal<bool>("return IsLeftAltKeyDown() and not GetCurrentKeyBoardFocus()", 0),
+                    new Action(ret =>
+                    {
+                        SpellManager.Cast("Heroic Leap");
+                        Lua.DoString("if SpellIsTargeting() then CameraOrSelectOrMoveStart() CameraOrSelectOrMoveStop() end");
+                        return;
+                    }));
+        }
+
+        private static Composite DemoBanner()
+        {
+            return
+                new Decorator(ret => SpellManager.CanCast("Demoralizing Banner") &&
+                    KeyboardPolling.IsKeyDown(Keys.Z),
+                    new Action(ret =>
+                    {
+                        SpellManager.Cast("Demoralizing Banner");
+                        Lua.DoString("if SpellIsTargeting() then CameraOrSelectOrMoveStart() CameraOrSelectOrMoveStop() end");
+                        return;
+                    }));
+        }
+
+        private static Composite MockingBanner()
+        {
+            return
+                new Decorator(ret => SpellManager.CanCast("Mocking Banner") &&
+                    KeyboardPolling.IsKeyDown(Keys.C),
+                    new Action(ret =>
+                    {
+                        SpellManager.Cast("Mocking Banner");
+                        Lua.DoString("if SpellIsTargeting() then CameraOrSelectOrMoveStart() CameraOrSelectOrMoveStop() end");
+                        return;
+                    }));
+        }
+
 
         #region WarriorTalents
         public enum WarriorTalents
